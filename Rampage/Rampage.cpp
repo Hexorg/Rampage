7f92d49c2524ab67520ba6128019e0deae62179a
Stanislav Ponomarev <sponomarev001@gmail.com>
2015-11-25 17:02:17 -0500

    Initial commit

---
+++ Rampage/Rampage.cpp
@@ -0,0 +1,15 @@
+/*
+ * Rampage.cpp
+ *
+ *  Created on: Nov 25, 2015
+ *      Author: hexorg
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
+
+
