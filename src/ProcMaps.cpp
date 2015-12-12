736954efcd811cb11de1b52cd15c6e741133d535
Stanislav Ponomarev <sponomarev001@gmail.com>
2015-12-12 11:35:50 -0500

    Added proper permission encoding to ProcMaps

--- src/ProcMaps.cpp
+++ src/ProcMaps.cpp
@@ -9,22 +9,33 @@ namespace System {
 		ss << "/proc/" << pid << "/maps";
 		std::ifstream mapFile(ss.str().c_str());
 		char buffer[1024];
+		char perms[4];
+		unsigned short dev_tmp;
 		while (mapFile.getline(buffer, sizeof(buffer))) {
 			ss.clear(); ss.str(buffer);
 			struct ProcMapEntry entry;
 			ss >> std::hex >> entry.start;
 			ss.ignore(3, '-');
-			ss >> std::hex >> entry.end >> std::skipws >>  entry.perms >> std::skipws  
-				>> std::hex >> entry.offset >> std::skipws >> std::hex >> entry.dev_major;
+			ss >> std::hex >> entry.end >> std::skipws >>  perms >> std::skipws  
+				>> std::hex >> entry.offset >> std::skipws >> std::hex >> dev_tmp;
 			ss.ignore(1, ':');
-			ss >> std::hex >> entry.dev_minor >> std::skipws >> std::dec >> entry.inode 
+			entry.perms = 0;
+			if (perms[3] == 'p') entry.perms |= PERM_PRIVATE;
+			if (perms[2] == 'x') entry.perms |= PERM_EXECUTE;
+			if (perms[1] == 'w') entry.perms |= PERM_WRITE;
+			if (perms[0] == 'r') entry.perms |= PERM_READ;
+			entry.dev_major = (unsigned char ) dev_tmp;
+			ss >> std::hex >> dev_tmp >> std::skipws >> std::dec >> entry.inode 
 				>> std::skipws >> entry.pathname;
+			entry.dev_minor = dev_tmp;
 			//mapFile.ignore(100, '\n');
 			entryList.push_back(entry);
 			if (entry.pathname.rfind(".so") != std::string::npos)
 				libraryList.push_back(entry);
 			else
 				nonLibraryList.push_back(entry);
+			if (entry.inode == 0)
+				nonFileList.push_back(entry);
 		}
 
 	}
@@ -43,4 +54,7 @@ namespace System {
 		return nonLibraryList;
 	}
 
+	std::vector<struct ProcMapEntry> ProcMaps::getNonFileEntries() const {
+		return nonFileList;
+	}
 }
--- src/Rampage.cpp
+++ src/Rampage.cpp
@@ -3,11 +3,14 @@
 
 int main(int argc, char *argv[]) {
 	System::ProcMaps fMap(3588);
-	std::vector<System::ProcMapEntry> result = fMap.getNonLibraryEntries();
+	std::vector<System::ProcMapEntry> result = fMap.getNonFileEntries();
 	std::vector<System::ProcMapEntry>::iterator it;
 
 	for (it = result.begin(); it != result.end(); ++it) {
-		std::cout << std::hex << it->start << '-' << it->end << ' ' << it->pathname << std::endl;
+		std::cout << std::hex << it->start << '-' << it->end << ' '
+			<< std::dec << (unsigned int) it->perms << ' ' 
+			<< std::hex << (unsigned int) it->dev_major << ':' << (unsigned int) it->dev_minor
+			<< ' ' << std::dec << it->inode << '\t' << it->pathname << std::endl;
 	}
 	return 0;
 }
